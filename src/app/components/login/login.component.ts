8e1d3c1799ff416b583bdc29592a1254e82938b4
emersonnobre <egrnobre@gmail.com>
2021-10-19 11:02:01 -0400

    login service and css

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoginService } from 'src/app/service/login.service';
 
@@ -12,9 +12,11 @@ import { LoginService } from 'src/app/service/login.service';
 export class LoginComponent implements OnInit {
 
   form: FormGroup = this.formBuilder.group({
-    email: '',
-    password: ''
+    email: ['', Validators.required],
+    password: ['', Validators.required]
   })
+
+  data!: any
   
   constructor(
     private formBuilder: FormBuilder, 
@@ -26,15 +28,61 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  showError(msg: String): void {
+    const divError: any = document.querySelector('.error')
+    divError.innerHTML = msg
+    divError.style.opacity = 1;
+    setTimeout(() => divError.style.opacity = 0, 2000)
+  }
+
+  showSuccess(msg: String): void {
+    const divSuccess: any = document.querySelector('.success')
+    divSuccess.innerHTML = msg
+    divSuccess.style.opacity = 1;
+    setTimeout(() => divSuccess.style.opacity = 0, 2000)
+  }
+
   submit() {
     let email = this.form.value.email
     let password = this.form.value.password
-    return this.httpClient.post<any>(`http://localhost:8080/login`, { email, password }).subscribe(res => {
-      console.log(res)
+    
+    if(this.form.valid) {
+      let response = this.loginService.login(email, password).subscribe(
+        () => {
+          this.showSuccess('Successfully loged in')
+        },
+        (error: any) => {
+          console.log(error.status)
+          if(error.status == 403) {
+            this.showError('Bad credentials')
+          }
+        }
+      )
+    } else {
+      this.showError('Invalid credentials')
+    }
+    
+  }
+
+  getUsers() {
+    const token = localStorage.getItem('token')
+    const divUsuarios = document.querySelector('.usuarios')
+
+    this.loginService.getUsers(token)?.subscribe(res => {
+      this.data = res
+      if(divUsuarios) this.data.forEach((element: any) => {
+        let htmlEl = document.createElement('p')
+        htmlEl.innerHTML = element.name.toString()
+        divUsuarios.appendChild(htmlEl)
+      });
     },
-    error => {
-      console.log(error)
+    (error: any) => {
+      this.showError(error)
     })
+  }
+
+  logout() {
+    this.loginService.logout()
   }
 
 }
--- src/app/service/login.service.ts
+++ src/app/service/login.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { User } from '../model/user';
+import { throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,17 +9,23 @@ export class LoginService {
 
   private apiUrl = 'http://localhost:8080'
 
-  private user: User = {
-    username: '',
-    password: ''
-  }
-
   constructor(private http: HttpClient) { }
 
   login(username: string, password: string) {
-    return this.http.post(`${this.apiUrl}/login`, {username, password}).subscribe((res) => {
-      localStorage.setItem('token', res.toString())
-    })
+    return this.http.post(`${this.apiUrl}/login`, {username, password}, { responseType: 'text' as 'json' })
+  }
+
+  getUsers(token: any) {
+    if(token) {
+      return this.http.get<any>(`${this.apiUrl}/api/users`, { headers: { Authorization: "Bearer " + token } })
+    } else {
+      return throwError('No token provided')
+    }
+  }
+
+  logout() {
+    localStorage.removeItem('token')
+    window.location.reload()
   }
 
 }
